8f479e92abced12e6a56373d256924873dd7b500
Fanner487 <eamontang1@hotmail.com>
2017-12-13 16:21:18 +0000

    sdfsdfd

--- scan/scan.py
+++ scan/scan.py
@@ -80,5 +80,5 @@ warped = four_point_transform(orig, screenCnt.reshape(4, 2) * ratio)
 print "STEP 3: Apply perspective transform"
 cv2.imshow("Original", imutils.resize(orig, height = 650))
 cv2.imshow("Scanned", imutils.resize(warped, height = 650))
-cv2.write("scan.png", imutils.resize(warped, height = 650))
+cv2.imwrite("scan.png", imutils.resize(warped, height = 650))
 cv2.waitKey(0)
