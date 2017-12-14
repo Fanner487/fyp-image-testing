f58d39b53584f98b7a607b1595e74283cc0a9b94
Fanner487 <eamontang1@hotmail.com>
2017-12-14 15:56:48 +0000

    blah

--- scan/image_example.py
+++ scan/image_example.py
@@ -1,18 +1,42 @@
 import picamera
+import threading
 import time
+from processing.qr import get_qr_code
+from processing.scan import scan_image
+
+
+def extract_code(file):
+    """thread worker function"""
+    # time.sleep(0.5)
+    print 'Thread for: ' + image
+
+    scan_image(file)
+    get_qr_code(file)
+    # print image
+    return
 
 camera = picamera.PiCamera()
 
 
-print(int(time.time()))
 
 while True:
 
 	time.sleep(1)
 
-	image_path = "images/" + str(int(time.time())) + ".jpg"
+	time_epoch = str(int(time.time()))
+	image_path = "images/" + time_epoch + ".jpg"
 
 	print "Taking photo: " + str(image_path)
 
 	camera.capture(image_path)
 
+	t = threading.Thread(target=extract_code, args=[time_epoch])
+	t.start()
+
+
+
+
+
+
+
+    
