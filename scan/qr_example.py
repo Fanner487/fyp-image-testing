cda4bdce2ba7693e89da1aa47f3b1898632598ee
Fanner487 <eamontang1@hotmail.com>
2017-12-17 10:08:36 +0000

    blah

--- scan/qr_example.py
+++ scan/qr_example.py
@@ -27,7 +27,7 @@ while True:
 	time.sleep(1)
 
 	time_epoch = str(int(time.time()))
-	image_path = "scans/" + time_epoch + ".jpg"
+	image_path = "scans/" + time_epoch + "-result.png"
 
 	print "Taking photo: " + str(image_path)
 
